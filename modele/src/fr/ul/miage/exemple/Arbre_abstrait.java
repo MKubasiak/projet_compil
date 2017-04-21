10d561ea7ada04588f42dd8dc5e0a8e4ce22e378
Legris <corentin.legris9@etu.univ-lorraine.fr>
2017-04-21 14:12:17 +0200

    Classe de l'arbre

--- modele/src/fr/ul/miage/exemple/Arbre_abstrait.java
+++ modele/src/fr/ul/miage/exemple/Arbre_abstrait.java
@@ -1,7 +1,24 @@
 package fr.ul.miage.exemple;
 
+import java.util.HashMap;
+
 public class Arbre_abstrait {
 	
+	private HashMap<Integer,String> listInstructions;
 	
+	private int nb_instructions;
+	
+	public Arbre_abstrait() {
+		nb_instructions = 0;
+	}
+	
+	public void construireArbre() {
+		
+	}
+	
+	public void ajouterInstruction(String instruction) {
+		listInstructions.put(nb_instructions, instruction);
+		nb_instructions = nb_instructions + 1;
+	}
 
 }
