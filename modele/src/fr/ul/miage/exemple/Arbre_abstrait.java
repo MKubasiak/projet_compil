90245e344e57548757276b40243a47bcf7212898
Legris <corentin.legris9@etu.univ-lorraine.fr>
2017-04-21 18:43:53 +0200

    Amélioration de la liste

--- modele/src/fr/ul/miage/exemple/Arbre_abstrait.java
+++ modele/src/fr/ul/miage/exemple/Arbre_abstrait.java
@@ -1,32 +1,37 @@
 package fr.ul.miage.exemple;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
 public class Arbre_abstrait {
 	
-	private HashMap<Integer,String[]> listInstructions;
+	private HashMap<Integer,ArrayList<String>> listInstructions;
 	
 	private int nb_instructions;
 	
 	public Arbre_abstrait() {
-		this.listInstructions = new HashMap<Integer,String[]>();
+		this.listInstructions = new HashMap<Integer,ArrayList<String>>();
 		this.nb_instructions = 0;
 	}
 	
 	public void construireArbre() {
 	}
 	
-	public void ajouterInstruction(String[] instruction) {
+	public void ajouterInstruction(ArrayList<String> instruction) {
 		listInstructions.put(nb_instructions, instruction);
 		nb_instructions = nb_instructions + 1;
 	}
 	
 	public void afficherListe() {
-		for(Entry<Integer, String[]> entry : listInstructions.entrySet()) {
+		for(Entry<Integer, ArrayList<String>> entry : listInstructions.entrySet()) {
 		    Integer cle = entry.getKey();
-		    String[] valeur = entry.getValue();
-		    System.out.println("Instruction : " + cle + "Valeurs : " + valeur.length);
+		    ArrayList<String> valeur = entry.getValue();
+		    System.out.println("---------------------------");
+		    System.out.println("Instruction : " + cle);
+		    for(int i = 0; i<valeur.size();i++) {
+		    	 System.out.println("  Valeurs : " + valeur.get(i));
+		    }
 		}
 	}
 
